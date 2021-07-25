f2084a1032729eb30db7f3cb447bc863828db39d
Dheeraj <meetdheerajreddy@gmail.com>
2021-07-25 21:18:48 +0530

    push

--- index.js
+++ index.js
@@ -101,6 +101,10 @@ client.on("message", async (message) => {
                     return;
                 if (msg.content.includes("you can't do this"))
                     return;
+                if (msg.content.includes("you cannot send another"))
+                    return;
+                if (msg.content.includes("Check the progress of your pet"))
+                    return;
                 rpgcount.adventure++;
             }
             else if (con.includes('farm')) {
